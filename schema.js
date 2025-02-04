a765fb071d1887e69efcd72ac09895b82af9c9c9
Hardyx07 <hardik.tailor@kalvium.community>
2025-02-04 11:06:22 +0530

    writing schema2

---
+++ schema.js
@@ -0,0 +1,63 @@
+const mongoose = new require('mongoose');
+
+/// BlogPost Schema
+const BlogPostSchema = new mongoose.Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+      },
+      content: {
+        type: String,
+        required: true,
+        minlength: 50,
+      },
+      author: {
+        type: String,
+        required: true,
+      },
+      tags: {
+        type: [String], 
+        default: [],
+      },
+      category: {
+        type: String,
+        default: "General",
+      },
+      likes: {
+        type: [String], 
+        default: [],
+      },
+      comments: [CommentSchema], 
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+      },
+    },
+    { timestamps: true } 
+  );
+
+
+  /// Comment Schema
+  const CommentSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    commentedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+
+  module.exports = mongoose.model('BlogPost', BlogPostSchema);
